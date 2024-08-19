6dfd2fd80ef45548a7c5aeab5c08bda9d979e949
Ritesh <riteshfyi@gmail.com>
2024-08-20 02:52:49 +0530

    deployment

--- notifier_api/api/index.js
+++ notifier_api/api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const mongoose = require("mongoose");
 const cron = require("node-cron");
-const sendEmail = require("./emailService");
+const sendEmail = require("../emailService");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
