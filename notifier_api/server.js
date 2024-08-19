26a1444a3d3ff856c36ce262925db7329f88649d
Ritesh <riteshfyi@gmail.com>
2024-08-20 02:36:30 +0530

    database_fixes

--- notifier_api/server.js
+++ notifier_api/server.js
@@ -7,10 +7,11 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 app.use(express.json());
 
 
-mongoose.connect(PORT, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
