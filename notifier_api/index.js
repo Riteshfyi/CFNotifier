f2c02a217a7d41136d23deb136940994836d2143
Ritesh <riteshfyi@gmail.com>
2024-08-20 16:00:30 +0530

    cors_fixes

--- notifier_api/index.js
+++ notifier_api/index.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
 const corsOptions = {
   origin: "https://cfnotifier.vercel.app",
   credentials: true,
@@ -102,6 +107,3 @@ cron.schedule("0 * * * *", async () => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
